08576d63c2a980588b65d372c2f16f76c78e435f
nikagamkrelidze <nikagamkrelidze233@gmail.com>
2019-01-22 11:03:11 +0400

    LIQ-1145 bug fix

--- client/Lykke.Service.IndexHedgingEngine.Client/Api/IPrimaryMarketApi.cs
+++ client/Lykke.Service.IndexHedgingEngine.Client/Api/IPrimaryMarketApi.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Lykke.Service.IndexHedgingEngine.Client.Models.PrimaryMarket;
+using Refit;
 
 namespace Lykke.Service.IndexHedgingEngine.Client.Api
 {
@@ -15,12 +16,14 @@ namespace Lykke.Service.IndexHedgingEngine.Client.Api
         /// Method to request general info about Primary Market.
         /// </summary>
         /// <returns>General info about Primary Market.</returns>
+        [Get("/api/PrimaryMarket/info")]
         Task<PrimaryMarketInfoModel> GetInfoAsync();
         
         /// <summary>
         /// Method to request Primary Market wallet balances.
         /// </summary>
         /// <returns>List of non-zero balances of Primary Market wallet.</returns>
+        [Get("/api/PrimaryMarket/balances")]
         Task<IReadOnlyList<PrimaryMarketBalanceModel>> GetBalancesAsync();
         
         /// <summary>
@@ -30,12 +33,14 @@ namespace Lykke.Service.IndexHedgingEngine.Client.Api
         /// <param name="amount">Amount to add or subtract from current balance (value can be negative).</param>
         /// <param name="userId">Backoffice user.</param>
         /// <param name="comment">A comment of why the change is performed.</param>
+        [Post("/api/PrimaryMarket/update")]
         Task ChangeBalance(string assetId, decimal amount, string userId, string comment);
         
         /// <summary>
         /// Method to request history of balance changes of Primary Market wallet.
         /// </summary>
         /// <returns>A list of balance changes of Primary Market wallet.</returns>
+        [Get("/api/PrimaryMarket/history")]
         Task<IReadOnlyList<PrimaryMarketBalanceChangeModel>> GetBalanceChangeHistoryAsync();
     }
 }
