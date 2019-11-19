65c258538afdadcceb704c82b01443afdb737d8c
Alexey Zelenin <alexey@zelenin.org>
2019-11-19 22:39:10 +0300

    Added CrossAssetPairSettingsService

---
+++ src/Lykke.Service.IndexHedgingEngine.Domain/Services/ICrossAssetPairSettingsService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lykke.Service.IndexHedgingEngine.Domain.Services
+{
+    public interface ICrossAssetPairSettingsService
+    {
+        Task<IReadOnlyCollection<CrossAssetPairSettings>> GetAll();
+    }
+}
---
+++ src/Lykke.Service.IndexHedgingEngine.DomainServices/Settings/CrossAssetPairSettingsService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Lykke.Service.IndexHedgingEngine.Domain;
+using Lykke.Service.IndexHedgingEngine.Domain.Repositories;
+using Lykke.Service.IndexHedgingEngine.Domain.Services;
+
+namespace Lykke.Service.IndexHedgingEngine.DomainServices.Settings
+{
+    [UsedImplicitly]
+    public class CrossAssetPairSettingsService : ICrossAssetPairSettingsService
+    {
+        private const string CacheKey = "key";
+
+        private readonly ICrossAssetPairSettingsRepository _crossAssetPairSettingsRepository;
+        private readonly InMemoryCache<CrossAssetPairSettings> _cache;
+
+        public CrossAssetPairSettingsService(ICrossAssetPairSettingsRepository crossAssetPairSettingsRepository)
+        {
+            _crossAssetPairSettingsRepository = crossAssetPairSettingsRepository;
+            _cache = new InMemoryCache<CrossAssetPairSettings>(settings => CacheKey, true);
+        }
+
+        public async Task<IReadOnlyCollection<CrossAssetPairSettings>> GetAll()
+        {
+            IReadOnlyCollection<CrossAssetPairSettings> crossAssetPairSettings = _cache.GetAll();
+
+            if (crossAssetPairSettings == null)
+            {
+                crossAssetPairSettings = await _crossAssetPairSettingsRepository.GetAllAsync();
+
+                _cache.Initialize(crossAssetPairSettings);
+            }
+
+            return crossAssetPairSettings;
+        }
+    }
+}
